a9860590c985105a7493a115947c5d4f518983e1
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-13 17:54:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,6 @@
 def using_concat(my_favorite_things, more_favs)
-  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
-  @all_my_favs = @my_favorite_things.concat(more_favs)
-end 
+  my_favorite_things.concat(more_favs)
+end
 
 def using_insert(list_of_programming_languages, another_language)
   @list_of_programming_languages = ["Ruby","JavaScript", "Java", "C#", "Objective C", "C++", "PHP"] 
