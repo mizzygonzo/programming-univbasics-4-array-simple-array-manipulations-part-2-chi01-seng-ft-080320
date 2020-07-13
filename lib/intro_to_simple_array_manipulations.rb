2b58d799251ea4940e0b4605c2cbb2645ec754e5
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-13 17:43:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,4 +4,4 @@ def using_concat(my_favorite_things, more_favs)
   @all_my_favs = @my_favorite_things.concat(more_favs)
 end 
 
-def using_insert()
+def using_insert(list_of_programming_languages, another_language)
