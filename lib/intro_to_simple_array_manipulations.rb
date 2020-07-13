9a586566a50fda78cbb5c900d558fe22b7d1523f
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-13 17:56:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,4 +8,8 @@ def using_insert(list_of_programming_languages, another_language)
   @list_of_programming_languages.insert(4, @another_language)
 end 
 
-def using_uniq 
+def using_uniq def using_insert(list_of_programming_languages, another_language)
+  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  another_language = "Python"
+  list_of_programming_languages.insert(4, another_language)
+end
